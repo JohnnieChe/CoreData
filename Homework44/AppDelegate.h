1f18140fc0a5a27094381d6cf108aa364e283f63
Евгений Чемоданов <eugenechemodanov@MacBook-Air-Evgenij.local>
2017-12-11 20:17:27 +0500

    Initial Commit

---
+++ Homework44/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Homework44
+//
+//  Created by Евгений Чемоданов on 11.12.2017.
+//  Copyright © 2017 Eugene Chemodanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Homework44/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Homework44
+//
+//  Created by Евгений Чемоданов on 11.12.2017.
+//  Copyright © 2017 Eugene Chemodanov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
